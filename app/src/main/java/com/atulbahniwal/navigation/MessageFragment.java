86c8c4115812497ab7ab48c963d047deba327612
Atul Bahniwal <atulbahniwal@Atuls-MacBook-Air.local>
2019-07-07 12:02:04 +0530

    new changes

--- app/src/main/java/com/atulbahniwal/navigation/MessageFragment.java
+++ app/src/main/java/com/atulbahniwal/navigation/MessageFragment.java
@@ -1,5 +1,6 @@
 package com.atulbahniwal.navigation;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +8,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MessageFragment extends Fragment {
+
+
+    public Button button;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_message,container, false);
+//        return inflater.inflate(R.layout.fragment_message,container, false);
+        View view = inflater.inflate(R.layout.fragment_message,container, false);
+
+
+        button = view.findViewById(R.id.button);
+        final Context context=getActivity();
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "hello", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        return view;
+
     }
+
+
+
 }
